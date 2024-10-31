e86acc1587ac3f7537600c1d6e329a9095b74d45
willock0 <williansilvaesouza04@gmail.com>
2024-10-31 18:09:11 -0300

    .
    .

---
+++ exercicios/ex com flex/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+function clickMenu(){
+
+       if (navegacao.style.display == 'none'){
+        navegacao.style = 'flex-flow: column'
+        navegacao.style.display = 'flex'
+        
+        }else {
+            navegacao.style.display = 'none'
+        }
+        
+}
